7530207e31add32a6cd8da5309636a55aeec6b25
Henry Siegler <hasiegler@gmail.com>
2026-02-08 13:48:13 -0800

    add dag dependencies

--- dags/main.py
+++ dags/main.py
@@ -2,7 +2,7 @@ from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
 from api.video_stats import get_playlist_id, get_video_ids, extract_video_data, save_to_json
-
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from datawarehouse.dwh import staging_table, core_table
 from dataquality.soda import yt_elt_data_quality
 
@@ -35,7 +35,7 @@ with DAG(
     description='A DAG to extract YouTube video stats and save to JSON',
     schedule='0 14 * * *',  # Daily at 14:00 UTC,
     catchup=False
-) as dag:
+) as dag_produce:
     
     # Define tasks
     playlistId = get_playlist_id()
@@ -43,34 +43,44 @@ with DAG(
     video_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(video_data)
 
+    trigger_update_db = TriggerDagRunOperator(
+        task_id='trigger_update_db',
+        trigger_dag_id='update_db'
+    )
+
     # Define task dependencies
 
-    playlistId >> video_ids >> video_data >> save_to_json_task
+    playlistId >> video_ids >> video_data >> save_to_json_task >> trigger_update_db
 
 with DAG(
     dag_id='update_db',
     default_args=default_args,
     description='A DAG to process JSON file and insert data into both staging and core schemas',
-    schedule='0 15 * * *',  # Daily at 15:00 UTC,
+    schedule=None,
     catchup=False
-) as dag:
+) as dag_update:
     
     # Define tasks
 
     update_staging = staging_table()
     update_core = core_table()
 
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id='trigger_data_quality',
+        trigger_dag_id='data_quality'
+    )
+
     # Define task dependencies
 
-    update_staging >> update_core
+    update_staging >> update_core >> trigger_data_quality
 
 with DAG(
     dag_id='data_quality',
     default_args=default_args,
     description='A DAG to check the data quality of staging and core schemas in the db',
-    schedule='0 16 * * *',  # Daily at 16:00 UTC,
+    schedule=None,
     catchup=False
-) as dag:
+) as dag_quality:
     
     # Define tasks
 
